6e077c0a4283921d25797264ef0a04e4e3b834be
spookmx <spookmx@gmail.com>
2015-10-16 17:59:00 -0500

    Removing bower compos now that they're ignored!

--- config/database.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-
-	// the database url to connect
-	//url : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu'
-    url : 'mongodb://analytics:4n4lyt1c5_n348o@ds037234.mongolab.com:37234/analytics'
-}
