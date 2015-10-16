f55bd9813a72c212567347bf49a6ef0267d564eb
spookmx <spookmx@gmail.com>
2015-10-16 18:34:28 -0500

    Adding some comments to the functionality

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-// set up ======================================================================
+// Initial Server Setup
 var express  = require('express');
-var app      = express(); 								// create our app w/ express
-var mongoose = require('mongoose'); 					// mongoose for mongodb
-var port  	 = process.env.PORT || 8080; 				// set the port
-var database = require('./config/database'); 			// load the database config
+var app      = express(); 	
+var mongoose = require('mongoose');
+var port  	 = process.env.PORT || 8080; 
+var database = require('./config/database');
 var morgan   = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
-// configuration ===============================================================
-mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
+// General configuration
+mongoose.connect(database.url);
 
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
-app.use(morgan('dev')); // log every request to the console
-app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'})); 
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(methodOverride('X-HTTP-Method-Override'));
 
 
-// routes ======================================================================
+// Establish supported routes on the server 
 require('./app/routes.js')(app);
 
-// listen (start app with node server.js) ======================================
+// Init listen for requests on the specified port
 app.listen(port);
 console.log("Server running on port: " + port);
